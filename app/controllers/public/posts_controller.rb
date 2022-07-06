cae7c3e24fe3534bd567a7a97217c7e7f57d73c3
EC2 Default User <kenya6910@icloud.com>
2022-07-06 02:47:52 +0000

    消失した分追記

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -9,25 +9,46 @@ class Public::PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to public_post_path
+    else
+      render "edit"
+    end
+  end
+
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.new(product_params)
+    @post = Post.new(post_params)
     @post.user_id = current_user.id
     # @post.genre_id = params[:post][:genre_id]
       if @post.save
-        redirect_to public_posts_path(@post)
+        redirect_to public_post_path(@post)
       else
         render :new
       end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to public_posts_path
+  end
+
   private
 
   def post_params
-    params.require(:product).permit(:title,:introduction,:image)
+    params.require(:post).permit(:title,:introduction,:image)
   end
 
 
