baa87786107ada5b196719886d6305059f03d9df
EC2 Default User <kenya6910@icloud.com>
2022-07-09 07:28:30 +0000

    MAP機能修正

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -50,10 +50,14 @@ class Public::PostsController < ApplicationController
     redirect_to public_posts_path
   end
 
+  def map
+    @posts = Post.all
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title,:introduction,:image,:comment,:genre_id)
+    params.require(:post).permit(:title,:introduction,:image,:comment,:genre_id,:latitude,:longitude)
   end
 
 
