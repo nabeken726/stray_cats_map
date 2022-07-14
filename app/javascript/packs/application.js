b0b3f23d0e30848eb9d4ecf8a6c8add6db608a54
EC2 Default User <kenya6910@icloud.com>
2022-07-14 13:08:26 +0000

    レイアウト調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,6 @@ import '@fortawesome/fontawesome-free/js/all'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
