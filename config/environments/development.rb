81b63dc80c06b2a2e47cf9da9d621eec40ce79d0
EC2 Default User <kenya6910@icloud.com>
2022-08-12 10:32:53 +0000

    お問合せ機能コメント追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -74,7 +74,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "c82bc55cd3f74be78f7ee28a7a1b22b2.vfs.cloud9.ap-northeast-1.amazonaws.com"
-
+  # キャッシュ利用化
+  config.action_mailer.perform_caching = true
   # お問合せ用
   # メール送信できなかったらエラー
   config.action_mailer.raise_delivery_errors = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,8 +117,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  # キャッシュ利用化
   config.action_mailer.perform_caching = true
   # お問合せ用
+  # このURLで利用ができるようにする
   config.action_mailer.default_url_options = { :host => 'http://52.69.12.232/' }
   # メール送信できなかったらエラー
   config.action_mailer.raise_delivery_errors = true
